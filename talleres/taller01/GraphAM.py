088871aa412286dcb285f90d9e834f039f4a2e80
Juan Esteban Jaramillo <juanesteban0607@live.com>
2022-01-25 19:38:57 -0500

    complementando taller 1

--- talleres/taller01/GraphAM.py
+++ talleres/taller01/GraphAM.py
@@ -1,17 +1,26 @@
+import numpy as np
 class GraphAm:
 
     def __init__(self, size):
-
+        self.matriz = np.zeros( (size,size))    
 
     def getEdges(self):
-
-
+        #aadaaaaa
+        return
+    
     def getWeight(self, source, destination):
-
+        return self.matriz[source][destination]
 
     def addArc(self, source, destination, weight):
+        self.matriz[source][destination] = weight
+               
 
     def getSuccessors(self, vertex):
-
+        filaVertice = self.matriz[vertex]
+        respuesta = []
+        for j in range(0,size):
+            if respuesta.append(j) != 0:
+                respuesta.append(j)
+            return respuesta
     def __str__(self):
 
