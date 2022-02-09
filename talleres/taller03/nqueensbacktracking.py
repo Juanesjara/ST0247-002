da82ce940481555928f89df93b32ffa543f029a5
Juan Esteban Jaramillo <juanesteban0607@live.com>
2022-02-08 20:54:47 -0500

    grafos y nqueens sin la lista de resultado

---
+++ talleres/taller03/nqueensbacktracking.py
@@ -0,0 +1,27 @@
+def seAtacanHastaI(tablero, i):
+    for j in range(0,i+1):
+        for k in range(j+1,i+1):
+            if abs(tablero[j]-tablero[k]) == abs(j-k) or tablero[j]==tablero[k]:
+                return True
+            return False
+
+def nreinas(n:int):
+    nreinasAUX(n,0,[0]*n)
+    
+def nreinasAUX(n:int,c:int,t:list):
+  if n == c:
+    print(t) 
+  else:
+    for f in range(n):
+      t[c] = f
+      if seAtacanHastaI(t,c):
+        pass
+      else:
+
+        nreinasAUX(n,c+1,t)
+
+def main():
+ nreinas(4)
+
+
+main()
